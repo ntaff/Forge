5e9c7e2523703385c198b05f4d441446f5509d20
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2019-03-24 13:24:27 +0100

    Create scrollto.js

---
+++ src/js/scrollto.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+      var page = $(this).attr('href'); // Page cible
+      var speed = 750; // Durée de l'animation (en ms)
+      $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+      return false;
+   });
+});
