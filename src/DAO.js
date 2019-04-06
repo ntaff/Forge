bc4f75bc9ffda42a0254f93d6877ddd2f2b3e732
Apakoh <quentin.bartschs@gmail.com>
2019-04-06 17:05:27 +0200

    DAO update

--- src/DAO.js
+++ src/DAO.js
@@ -1,6 +1,10 @@
+// Global Variable
+var burgerKing = {};
+var macdonald;
+
 // Connection to DataBase
 module.exports = {
-    DAO: function() {
+    DAO: function(bdd_name) {
         return new Promise(resolve => {
             setTimeout(() => {
                 var MongoClient = require('mongodb').MongoClient;
@@ -8,42 +12,76 @@ module.exports = {
                 var client = new MongoClient(uri, { useNewUrlParser: true });
 
                 client.connect(err => {
-                    var bk = client.db("Forge").collection("Burger King's");
-                    var mc = client.db("Forge").collection("Macdonald's");
-
-                    console.log("Connection to MongoDB Atlas DataBase Forge succefull");
-
-                    client.close();
-                    resolve(bk);
+                    // "Macdonald's" or "Burger King's"
+                    var bdd = client.db("Forge").collection(bdd_name);
+                    console.log("Connection to MongoDB Atlas DataBase Forge succefull for " + bdd_name);
+                    resolve([bdd,client]);
                 });
             }, 0);
         });
     },
 
-    Macdonald: function(macdonald) {
+    Macdonald: async function() {
+        var bdd = await module.exports.DAO("Macdonald's");
+        var macdonald = bdd[0];
+        var client = bdd[1];
         var query = {};
         const cursor = macdonald.find(query);
 
-        cursor.each(function(err, item) {
-            if(item != null) {
-              console.log(item);
-            }
-        });
+        var tab = [];
+
+        while(await cursor.hasNext())
+        {
+          const doc = await cursor.next();
+          tab.push({Longitude: doc.Longitude, Latitude: doc.Latitude});
+          // console.log(doc.Longitude)
+          // Definition of a time between each line
+        }
+
+        // Do not forget to close the connection
+        client.close();
+
+        return new Promise(resolve => {
+            setTimeout(() => {
+                    resolve(tab);
+                });
+            }, 0);
+
     },
 
-    BurgerKing: function(burgerKing) {
+    BurgerKing: async function() {
+        var bdd = await module.exports.DAO("Burger King's");
+        var burgerKing = bdd[0];
+        var client = bdd[1];
         var query = {};
-        const cursor = burgerKing.find(query);
+        const cursor = await burgerKing.find(query);
 
-        cursor.each(function(err, item) {
-            if(item != null) {
-              console.log(item);
-            }
-        });
+        var tab = [];
+
+        while(await cursor.hasNext())
+        {
+          const doc = await cursor.next();
+          tab.push({Longitude: doc.Longitude, Latitude: doc.Latitude});
+          // console.log(doc.Longitude)
+          // Definition of a time between each line
+        }
+
+        // Do not forget to close the connection
+        client.close();
+
+        return new Promise(resolve => {
+            setTimeout(() => {
+                    resolve(tab);
+                });
+            }, 0);
     },
 
     Test: async function() {
-        var burgerKing = await module.exports.DAO();
-        console.log(typeof burgerKing);
+        var mc = await module.exports.Macdonald();
+        var bk = await module.exports.BurgerKing();
+        console.log(mc);
+        console.log(bk);
     }
 }
+
+module.exports.Test();
