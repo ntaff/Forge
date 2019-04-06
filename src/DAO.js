87e3abfd5c8cb5b6b003ca2ea84224047ad3a88c
Apakoh <quentin.bartschs@gmail.com>
2019-04-06 12:37:05 +0200

    Model critical update

--- src/DAO.js
+++ src/DAO.js
@@ -1,56 +1,49 @@
-var burgerKing;
-var macdonald;
+// Connection to DataBase
+module.exports = {
+    DAO: function() {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                var MongoClient = require('mongodb').MongoClient;
+                var uri = "mongodb+srv://Nicolas:Nicolas@forge-kmw2f.gcp.mongodb.net/test?retryWrites=true";
+                var client = new MongoClient(uri, { useNewUrlParser: true });
 
-// Conncetion to DataBase
-export function DAO()
-{
-    const MongoClient = require('mongodb').MongoClient;
-    // Replace user and <password>
-    const uri = "mongodb+srv://Nicolas:Nicolas@forge-kmw2f.gcp.mongodb.net/test?retryWrites=true";
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    await client.connect(err => {
-      const bk = client.db("Forge").collection("Burger King's");
-      const mc = client.db("Forge").collection("Macdonald's");
-      console.log("Connection to MongoDB Atlas DataBase Forge succefull");
-      client.close();
-      return bk;
-    });
+                client.connect(err => {
+                    var bk = client.db("Forge").collection("Burger King's");
+                    var mc = client.db("Forge").collection("Macdonald's");
+
+                    console.log("Connection to MongoDB Atlas DataBase Forge succefull");
+
+                    client.close();
+                    resolve(bk);
+                });
+            }, 0);
+        });
+    },
+
+    Macdonald: function(macdonald) {
+        var query = {};
+        const cursor = macdonald.find(query);
+
+        cursor.each(function(err, item) {
+            if(item != null) {
+              console.log(item);
+            }
+        });
+    },
+
+    BurgerKing: function(burgerKing) {
+        var query = {};
+        const cursor = burgerKing.find(query);
+
+        cursor.each(function(err, item) {
+            if(item != null) {
+              console.log(item);
+            }
+        });
+    },
+
+    Test: async function() {
+        var burgerKing = await module.exports.DAO();
+        console.log(typeof burgerKing);
+    }
 }
-
-
-// Queries to DataBase
-function Macdonald()
-{
-    var query = {};
-    const cursor = macdonald.find(query);
-    cursor.each(function(err, item)
-    {
-        if(item != null)
-        {
-          console.log(item);
-        }
-    });
-}
-
-
-function BurgerKing()
-{
-    var query = {};
-    const cursor = burgerKing.find(query);
-    cursor.each(function(err, item)
-    {
-        if(item != null)
-        {
-          console.log(item);
-        }
-    });
-}
-
-async function Test()
-{
-  burgerKing = await DAO();
-  //BurgerKing();
-  console.log(burgerKing);
-}
-
-Test();
--- src/server.js
+++ src/server.js
@@ -1,28 +1,31 @@
-var express = require('express');
-var server = express();
-var cars = ["Saab", "Volvo", "BMW"];
+const express = require('express');
+const server = express();
+const dao = require('./DAO.js');
 
 server.use("/css", express.static(__dirname + '/css'));
 server.use("/images", express.static(__dirname + '/images'));
 server.use("/js", express.static(__dirname + '/js'));
+server.use(".././data", express.static(__dirname + '../data'));
 
 server.engine('html', require('atpl').__express);
 server.set('devel', false);
 server.set('view engine', 'html');
-server.set('view cache', true);
+server.set('view cache', false);
 server.set('views', __dirname);
 
 server.get('/', function(req, res){
-  res.render('index.html', {message:"coucou", cars: cars});
+  res.render('index.html', {message:"coucou"});
 });
 
 server.get('/index.html', function(req, res){
-  res.render('index.html', {message:"coucou", cars: cars});
+  res.render('index.html', {message:"coucou"});
 });
 
 server.get('/carte.html', function(req, res){
-  res.render('carte.html', {message:"coucou"});
+  // {{ Variable }} in html to display parameters sent to html page
+  res.render('carte.html', {message:"coucou", dao: dao.BurgerKing(dao.Test())});
 });
 
 server.listen(3000, "0.0.0.0", function() {
     console.log('Listening to port:  ' + 3000);
+});
